f388702f0d0bd63a45ca27fc044bd235152615e5
joseluisgs <joseluisgonsan@gmail.com>
2020-05-25 09:12:52 +0200

    directivas inicial

---
+++ pages/directivas.vue
@@ -0,0 +1,18 @@
+<template>
+  <p>Hola {{ mensaje }}</p>
+</template>
+
+<script>
+export default {
+  // La función data nos sirve para enlazar los datos de la logica en las vistas. Devuelve los elementso que rendirezaremos con {{}}
+  data () {
+    return {
+      mensaje: 'datos datos'
+    };
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
