151c5c644d946a3049daccec71bb33136622c3e1
joseluisgs <joseluisgonsan@gmail.com>
2020-05-25 13:58:15 +0200

    Petición POST

--- components/Slogan.vue
+++ components/Slogan.vue
@@ -3,14 +3,45 @@
     <h2 class="title is-2">
       FoodAdvisor, tu web de restaurantes
     </h2>
-    <div class="subcribe">
-      <input type="text" class="input email" placeholder="email...">
-      <button class="button is-info">
+    <!--Si no esta suscrito mostramos todo esto -->
+    <div v-if="!is_subscribe" class="subcribe">
+      <!--Tomamos el campo email -->
+      <input v-model="email" type="text" class="input email" placeholder="email...">
+      <button class="button is-info" @click="subscribe">
         Suscribirse
       </button>
     </div>
+    <div v-else>
+      <p>Gracias! Ahora encontrás cada semana una recomendación en tu email.</p>
+    </div>
   </div>
 </template>
+
+<script>
+import api from '@/services/api';
+export default {
+  // Datos de nuestro componente
+  // Métodos
+  data () {
+    return {
+      email: '',
+      is_subscribe: false
+    };
+  },
+  methods: {
+    // Cuando pulse el botón llamamos a este evento con la PI pasandole los datos. en el payload
+    async subscribe () {
+      const payload = {
+        email: this.email
+      };
+      const response = await api.postSubcribeUser(payload);
+      if (response.status === 201) { // 201 : Created
+        this.is_subscribe = true;
+      }
+    }
+  }
+};
+</script>
 
 <style scoped>
 .subcribe{
--- services/api.js
+++ services/api.js
@@ -24,9 +24,34 @@ async function apiGet (url) {
   }
 }
 
+// Petición POST Genérica
+async function apiPost (url, payload) {
+  const config = {
+    method: 'get',
+    mode: 'cors',
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    },
+    body: payload
+  };
+  const urlCall = API_URL + url;
+  try {
+    const response = await axios.post(urlCall, config);
+    console.log(response);
+    return response;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
 // GET de Restaurantes
 api.getRestaurants = () => {
   return apiGet('restaurants');
+};
+
+// POST Susucribir usuario
+api.postSubcribeUser = (payload) => {
+  return apiPost('users', payload);
 };
 
 export default api;
