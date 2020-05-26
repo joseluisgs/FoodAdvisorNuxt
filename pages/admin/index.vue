ca30a1bb9fed94b1570a9c63ffd0d09c32ad7ffb
joseluisgs <joseluisgonsan@gmail.com>
2020-05-26 12:51:49 +0200

    Firebase Get Restaurant

---
+++ pages/admin/index.vue
@@ -0,0 +1,76 @@
+<template>
+  <div>
+    <section class="section">
+      <div class="columns">
+        <div class="column">
+          <h2 class="title is-2">
+            Resturantes
+          </h2>
+        </div>
+      </div>
+      <!-- Creamos una tabla -->
+      <div class="table-container">
+        <table class="table is-fullwidth is-bordered">
+          <thead>
+            <tr>
+              <th>Id</th>
+              <th>Name</th>
+              <th>Category</th>
+              <th>City</th>
+              <th>Image</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            <!-- Por cada restaurante lo recorremos en el for -->
+            <tr v-for="(item, index) in restaurants" :key="index">
+              <td>{{ item.id }}</td>
+              <td>{{ item.name }}</td>
+              <td>{{ item.category }}</td>
+              <td>{{ item.city }}</td>
+              <td>
+                <img :src="item.image" alt="Pizza Restaurant" width="200px">
+              </td>
+              <td>
+                <!-- añadimos el botón para editar que redirecciona al elemento con ese id -->
+                <nuxt-link class="button" :to="item.id">
+                  Edit
+                </nuxt-link>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </section>
+  </div>
+</template>
+<script>
+// Importamos la bd
+import { db } from '@/plugins/firebase';
+export default {
+  // Nuestro modelo de datos
+  data () {
+    return {
+      restaurants: []
+    };
+  },
+  // En el estado created de nuestro componente consultamos la BD firebase
+  created () {
+    // Hacemos el get
+    const response = db.collection('restaurants').get();
+    response
+      .then((snapshot) => {
+        snapshot.forEach((doc) => {
+          const restaurant = {
+            id: doc.id,
+            ...doc.data() // descompone el documento que nos llega en sus propiedades y las enlaza en el nuevo objeto que estamos creado
+          };
+          this.restaurants.push(restaurant); // Lo metemos en nuestra variable
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+};
+</script>
